9b3b6ca7bd6515547e8cebaccd282d168842d37d
bramandityo <freez.meinster@gmail.com>
2019-04-05 03:08:23 +0700

    nguknguk1

--- scripts/newapp.py
+++ scripts/newapp.py
@@ -4,13 +4,6 @@ from django.core.management import call_command
 
 APP_TEMPLATES = next(os.walk(os.path.join(settings.BASE_DIR, "data/app_templates")))[1]
 
-def check_target(name):
-    target_path = os.path.join(settings.BASE_DIR, "apps/%s" % name)
-    if not os.path.isdir(target_path):
-        return True
-    else:
-        return False
-
 def check_name_conflick(name):
     apps_list = next(os.walk(os.path.join(settings.BASE_DIR, "apps")))[1]
     apps_list = apps_list +['admin', 'admindocs', 'auth' ,'contenttypes' ,'flatpages','gis','humanize',
@@ -24,7 +17,7 @@ def check_name_conflick(name):
 def run():
     print("========== Hello to app creator ========== \n")
     name = input("Apps name: ")
-    if check_name_conflick(name) or not check_target(name):
+    if check_name_conflick(name):
         print("Sorry, but you can't use %s as name because this name already taken" % name)
         exit()
 
